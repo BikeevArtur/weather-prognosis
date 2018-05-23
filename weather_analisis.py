2408b6231b82119ae1a5800ee87d5b1d12ccd6d8
BikeevArtur <37339260+BikeevArtur@users.noreply.github.com>
2018-05-23 18:05:14 +0300

    Update weather_analisis.py
    Добавлена круговая диаграмма типа погоды. Сначала показывается график температур, затем круговая диаграмма

--- weather_analisis.py
+++ weather_analisis.py
@@ -1,17 +1,34 @@
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import defaultdict
 
 
 def temperature_graphics(dictionery):
-    l = []
+    temperature = []
     for d in dictionery['list']:
-        l.append(d["main"]['temp'])
-    k = np.arange(0, len(l), 1)
-    plt.plot(k, l, "b:")
+        temperature.append(d["main"]['temp'])
+    k = np.arange(0, len(temperature), 1)
+    plt.plot(k, temperature, "b:")
     plt.title('График температуры', fontweight='normal', color='r', fontsize=20)
     plt.xlabel('Час', fontweight='normal', fontsize=14)
     plt.ylabel('Температура', fontweight='normal', fontsize=14)
+    plt.show()
+
+
+
+def pie_chart(dictionery):
+    weather = defaultdict(lambda: 0)
+    for d in dictionery["list"]:
+        weather[d["weather"][0]["description"]] += 1
+    k = []
+    l = []
+    for w in weather:
+        l.append(w)
+        k.append(weather[w])
+    plt.title('Распространенность типа погоды', fontweight='normal', color='r', fontsize=20)
+    plt.pie(k, autopct='%.1f', radius=1.0, explode=[0.2] + [0 for _ in range(len(l) - 1)])
+    plt.legend(bbox_to_anchor=(-0.16, 0.45, 0.25, 0.25), loc='lower left', labels=l)
     plt.show()
 
 
@@ -23,3 +40,4 @@ with open(corpus, 'r',  encoding="utf-8") as fout:
 
 dictionery = json.loads(s)
 temperature_graphics(dictionery)
+pie_chart(dictionery)
